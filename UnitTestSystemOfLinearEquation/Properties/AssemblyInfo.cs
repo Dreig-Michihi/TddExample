f3938c66997b0b843cbafa3dbc1c7ff6a41934b9
Roman Shamov <dreigfire@mail.ru>
2021-02-04 01:53:41 +0300

    Forgot SLE. Continued project. Added Tests for constructor, and get set in this[] override.

---
+++ UnitTestSystemOfLinearEquation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSystemOfLinearEquation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSystemOfLinearEquation")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19a05807-e37b-45a8-84a8-0badae10ffc9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestSystemOfLinearEquation/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Task2_cs;
+
+namespace UnitTestSystemOfLinearEquation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IntConstructor()
+        {
+            int m = 3;
+            SystemOfLinearEquation sle = new SystemOfLinearEquation(2);
+            Assert.AreEqual("0=0\n0=0\n", sle.ToString());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IntConstructorWithNegativeArgumet()
+        {
+            int m = -2;
+            Assert.Equals(typeof(ArgumentException), new SystemOfLinearEquation(m));
+        }
+        [TestMethod]
+        public void Getter()
+        {
+            SystemOfLinearEquation sle = new SystemOfLinearEquation(1);
+            Assert.AreEqual("0=0", sle[0].ToString());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetterWithWrongArgument()
+        {
+            SystemOfLinearEquation sle = new SystemOfLinearEquation(1);
+            Assert.Equals(typeof(ArgumentException), sle[-4].ToString());//может тут бы подошел и OutOfRangeException?
+        }
+        [TestMethod]
+        public void SetterAndGetter()
+        {
+            SystemOfLinearEquation sle = new SystemOfLinearEquation(2);
+            sle[1] = new LinearEquation(new double[] { 4, 5, 2 });
+            Assert.AreEqual("2x2+5x1+4=0", sle[1].ToString());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetterWithWrongArgument()
+        {
+            SystemOfLinearEquation sle = new SystemOfLinearEquation(2);
+            Assert.Equals(typeof(ArgumentException), sle[3] = new LinearEquation(new double[4] { 1, 2, 3, 4 }));//может тут бы подошел и OutOfRangeException?
+        }
+    }
+}
