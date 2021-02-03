c9c9fc34f8884731c18f6e11157273e374e73ea8
Roman Shamov <dreigfire@mail.ru>
2021-02-03 14:39:40 +0300

    commit

---
+++ Test1_cs/Indexer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_cs
+{
+    public class Indexer
+    {
+        double[] arr;
+        int startIndex, endIndex;
+        public Indexer(double[] arr, int startIndex, int endIndex)
+        {
+
+        }
+        public int Length
+        {
+            get => endIndex - startIndex + 1;
+        }
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Length)
+                    throw new IndexOutOfRangeException();
+                return arr[index+startIndex];
+            }
+            set
+            {
+                if (index < 0 || index >= Length)
+                    throw new IndexOutOfRangeException();
+                arr[index + startIndex] = value;
+            }
+        }
+    }
+}
---
+++ UnitTestIndexer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestIndexer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestIndexer")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("507825c9-a1cf-4853-8512-f926ed6534c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
